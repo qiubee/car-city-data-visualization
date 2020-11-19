664b5b65235c96a85199c67b5f241427e732fd01
qiubee <45637654+qiubee@users.noreply.github.com>
2020-11-19 17:18:26 +0100

    update projection config & add topojson

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,8 +1,8 @@
 createChoroplethMap();
 
 async function createChoroplethMap() {
-	const path = setupMap(800, 800);
-	const provincesData = await loadJSON("data/parking_provinces.json");
+	const path = setupMap(600, 600);
+	const provincesData = await loadJSON("data/parking_provinces_topo.json");
 	drawMap(path, provincesData);
 }
 
@@ -12,7 +12,17 @@ function setupMap(width, height) {
 		.attr("width", width)
 		.attr("height", height);
 	
-	const projection = d3.geoMercator();
+	const scale = width * height / 50;
+	const trans_left = width % height !== 0 ? 
+		-width % scale / (scale / 12500) + (width % height * 0.33) :
+		-width % scale / (scale / 12500);
+	console.log(scale, trans_left);
+
+	const projection = d3.geoMercator()
+		.rotate([5.38763888888889, 0])
+		.center([0, 52.15616055555555])
+		.scale(scale)
+		.translate([trans_left, height / 2]);
 	const path = d3.geoPath().projection(projection);
 	return path;
 }
@@ -26,10 +36,12 @@ function drawMap(path, data) {
 		.enter()
 		.append("path")
 		.attr("d", path)
-		.attr("fill", "#000");
+		.attr("stroke", "#77bb00")
+		.attr("fill", "#fbfbfb");
+
 }
 
 async function loadJSON(path) {
 	const data = await d3.json(path);
-	return data;
+	return topojson.feature(data, data.objects);
 }
