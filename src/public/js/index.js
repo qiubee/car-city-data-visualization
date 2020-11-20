19e24854cd52adf160c18f9914c0479a3c5d3dc1
qiubee <45637654+qiubee@users.noreply.github.com>
2020-11-20 20:28:54 +0100

    filter data on select change & update map

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -13,7 +13,8 @@ function setupMap(width, height) {
 		.insert("svg", ":first-child")
 		.attr("viewBox", "0 0 " + 600 + " " + 600)
 		.attr("width", width)
-		.attr("height", height);
+		.attr("height", height)
+		.append("g");
 	
 	const scale = width * height / 50;
 	const transLeft = width % height !== 0 ? 
@@ -29,10 +30,15 @@ function setupMap(width, height) {
 	return path;
 }
 
-function drawMap(path, data) {
-	const map = d3.select("#map svg")
-		.append("g");
+function drawMap(path, data, node=null) {
+	const selected = node ? 
+		node.selectedOptions.item(0).dataset.key : 
+		"parkingTotal";
+	const selectedData = filterOnKey(data.features, selected);
 
+	const map = d3.select("#map g");
+	const select = d3.select("#map form select");
+		
 	const color = d3.scaleSequentialQuantile([
 		[1000, 5000, 100000],
 		[d3.interpolateBlues()]
@@ -44,18 +50,20 @@ function drawMap(path, data) {
 		.append("path")
 		.attr("d", path)
 		.attr("stroke", "#ffffff")
+		.data(selectedData)
 		.attr("fill", function (d) {
-			console.log(color(d.properties.parkingTotal));
-		});
-		
+			return color(d);
+		})
+		.exit().remove();
+	
+	select.on("change", function (event) {
+		drawMap(path, data, event.target);
+	});
 }
 
 function addForm(data) {	
 	const descriptions = ["Totaal aantal parkeerplaatsen", "Totaal aantal parkeerplaatsen die het hele jaar open zijn", "Totaal aantal parkeerplaatsen met de uitgang altijd open"];
-	const names = Object.keys(createCategories(data, descriptions))
-		.map(function (d) {
-			return d;
-		});
+	const names = createCategories(data);
 
 	const select = d3.select("#map")
 		.append("form")
@@ -69,25 +77,25 @@ function addForm(data) {
 		.enter()
 		.append("option")
 		.attr("value", function (d) {
+			return d.toLowerCase();
+		})
+		.attr("data-key", function (d) {
 			return d;
 		})
 		.data(descriptions)
 		.text(function (d) {
-			return d.toLowerCase();
+			return d;
 		});
-	
-
-
 }
 
-function createCategories(data, descriptions) {
+function filterOnKey(data, keyToFilter) {
 	return data.map(function (feature) {
 		return Object.entries(feature.properties)
 			.reduce(function (acc, property) {
 				const key = property[0];
 				const value = property[1];
-				if (typeof value === "number") {
-					acc.push(key.toLowerCase());
+				if (key === keyToFilter && typeof value === "number") {
+					acc.push(value);
 				}
 				return acc;
 			}, []);
@@ -95,17 +103,41 @@ function createCategories(data, descriptions) {
 		return acc.concat(arr);
 	}).filter(function (a, b, arr) {
 		return arr.indexOf(a) === b;
-	}).reduce(function (acc, name, i) {
-		for (let j = 0; j < descriptions.length; j++) {
-			const description = descriptions[j];
-			if (i === j) {
-				Object.assign(acc, {
-					[name]: description
-				});
+	});
+}
+
+function createCategories(data, descriptions=undefined) {
+	const values = data.map(function (feature) {
+		return Object.entries(feature.properties)
+			.reduce(function (acc, property) {
+				const key = property[0];
+				const value = property[1];
+				if (typeof value === "number") {
+					acc.push(key);
+				}
+				return acc;
+			}, []);
+	}).reduce(function (acc, arr) {
+		return acc.concat(arr);
+	}).filter(function (a, b, arr) {
+		return arr.indexOf(a) === b;
+	});
+	
+	if (descriptions) {
+		return values.reduce(function (acc, name, i) {
+			for (let j = 0; j < descriptions.length; j++) {
+				const description = descriptions[j];
+				if (i === j) {
+					Object.assign(acc, {
+						[name]: description
+					});
+				}
 			}
-		}
-		return acc;
-	}, {});
+			return acc;
+		}, {});
+	} else {
+		return values;
+	}
 }
 
 async function loadJSON(path) {
